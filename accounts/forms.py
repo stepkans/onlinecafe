493c49120037c66a0f55feb901f86fa40a216b07
Stephen Suyanka <stepkans@gmail.com>
2023-09-17 17:30:24 +0300

    Vendor profile form plus validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import MyUser
+from .models import MyUser, UserProfile
+from .validators import allow_only_images_validator
 
 class MyUserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -18,3 +19,20 @@ class MyUserForm(forms.ModelForm):
                 raise forms.ValidationError("Password does not match!")    
         else: 
             raise forms.ValidationError("Passwords can't be blank.")       
+        
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    
+    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+
+    class Meta:
+        model =  UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address' , 'country', 'state', 'city','pin_code', 'latitude', 'longitude']     
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs) 
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'  
